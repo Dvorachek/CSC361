d12751f5e9fc665973cb59210be1a6d9586ef052
Dylan <Dylan.Dvorachek@gmail.com>
2018-01-12 23:15:54 -0800

    attempts http before https

--- a1/SmartClient.py
+++ a1/SmartClient.py
@@ -1,78 +1,93 @@
 import socket
 import ssl
 import sys
+import re
+
 
 _BUFF_SIZE = 10000
+
+
 class smart_web_client(object):
-    
-    
+
     def __init__(self, host, port):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.settimeout(10)
-        self.s.connect((host, port))
-        self.ss = ssl.wrap_socket(self.s, keyfile=None, certfile=None, server_side=False, cert_reqs=ssl.CERT_NONE, ssl_version=ssl.PROTOCOL_SSLv23)
+        # self.secure = self.__check_https(host)
         self.host = host
         self.port = port
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.settimeout(10)
+        
+        if self.__check_https(host):
+            self.__set_https()
+            # self.s = ssl.wrap_socket(self.s, keyfile=None, certfile=None, server_side=False, cert_reqs=ssl.CERT_NONE, ssl_version=ssl.PROTOCOL_SSLv23)
+        self.connect()
         
     def connect(self):
-        self.ss.connect((self.host, self.port))
+        self.s.connect((self.host, self.port))
         
     def disconnect(self):
-        self.ss.close
+        self.s.close
 
     def send_request(self, request):
     
-        # print request
-        self.ss.sendall(request)
+        self.s.sendall(request)
         
-        response = self.ss.recv(_BUFF_SIZE)
+        response = self.s.recv(_BUFF_SIZE)
         r = response
-        # print response
 
         while response:
-            response = self.ss.recv(_BUFF_SIZE)
+            response = self.s.recv(_BUFF_SIZE)
             r += response
-        return r 
+            
+        if r:
+            return r
+        else:
+            self.__set_https()
+            self.connect()
         
+    def __set_https(self):
+        self.s = ssl.wrap_socket(self.s, keyfile=None, certfile=None, server_side=False, cert_reqs=ssl.CERT_NONE, ssl_version=ssl.PROTOCOL_SSLv23)
+        
+    def __check_https(self, host):
+        return True
+        print host
+        if re.match(r"https://.+", host):
+            
+            print 'yes'
+            return True
+        print 'no'
+        return False
+
+
 def main():
-    host = "https://www.uvic.ca/"
-    host1 = 'www.github.com'
+    host = sys.argv[1]
+    #TESTS
+    host1 = 'github.com'
     host2 = 'www.uvic.ca'
-    # h = host.split('/')[2]
-    
+    request1 = "HEAD / HTTP/1.0\r\nHost: github.com\r\nConnection: close\r\n\r\n"
+    request2 = "HEAD / HTTP/1.0\r\nHost: www.uvic.ca\r\nConnection: close\r\n\r\n"
+
     port = 80
     port = 443
-    # request = b"GET / HTTP/1.0\nHost: {}\r\n\r\n".format(h)
 
-    r1 = "HEAD / HTTP/1.0\r\nHost: www.github.com\r\nConnection: close\r\n\r\n"
-    
-    r2 = "HEAD / HTTP/1.0\r\nHost: www.uvic.ca\r\nConnection: close\r\n\r\n"
+    request = "HEAD / HTTP/1.0\r\nHost: {}\r\nConnection: close\r\n\r\n".format(host)
 
-    # try:
-    client = smart_web_client(host1, port)
-    # except:
-        # print "failed creating socket"
+    client = smart_web_client(host, port)
+
+    response = client.send_request(request)
     
-    # try:
-    # client.connect()
-    # except:
-        # print "failed connecting to {} on {}".format(host, port)
-    # try:
-    response = client.send_request(r1)
-    print response
-    response = response.split(' ')[1]
+    if not response:
+        client.__set_https()
+        client.connect()
+        response = client.send_request(request)
+        
     print response
     
-    # print 'test'
-    # except Exception, e:
-       # print str(e)
-    
-    # try:
+    # response = response.split(' ')[1]
+
+    print 'end'
+
     client.disconnect()
-    # except Exception, e:
-       # print str(e)
 
 
 if __name__=='__main__':
     main()
-
