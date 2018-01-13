fef1c207bf6646a385408c453fc7a04c55197919
Dylan <Dylan.Dvorachek@gmail.com>
2018-01-12 17:56:30 -0800

    now need to parse info

--- a1/SmartWebClient.py
+++ a1/SmartWebClient.py
@@ -8,7 +8,9 @@ class smart_web_client(object):
     
     def __init__(self, host, port):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.ss = ssl.wrap_socket(self.s, ssl_version=ssl.PROTOCOL_TLSv1)
+        self.s.settimeout(10)
+        self.s.connect((host, port))
+        self.ss = ssl.wrap_socket(self.s, keyfile=None, certfile=None, server_side=False, cert_reqs=ssl.CERT_NONE, ssl_version=ssl.PROTOCOL_SSLv23)
         self.host = host
         self.port = port
         
@@ -19,14 +21,14 @@ class smart_web_client(object):
         self.ss.close
 
     def send_request(self, request):
+    
         print request
-        self.ss.send(request)
+        self.ss.sendall(request)
+        
         response = self.ss.recv(_BUFF_SIZE)
         r = response
         print response
-        
-        
-        t = response
+
         while response:
             response = self.ss.recv(_BUFF_SIZE)
             r += response
@@ -34,27 +36,30 @@ class smart_web_client(object):
         
 def main():
     host = "https://www.uvic.ca/"
-    h = host.split('/')[2]
+    host1 = 'github.com'
+    host2 = 'www.uvic.ca'
+    # h = host.split('/')[2]
     
     port = 80
-    request = b"GET / HTTP/1.0\nHost: {}\r\n\r\n".format(h)
+    port = 443
+    # request = b"GET / HTTP/1.0\nHost: {}\r\n\r\n".format(h)
+
+    r1 = "GET / HTTP/1.0\r\nHost: github.com\r\nConnection: close\r\n\r\n"
     
-    
-    
-    r2 = b"GET / HTTP/1.0\nHost: uvic.ca\n\n"
+    r2 = "GET / HTTP/1.0\r\nHost: www.uvic.ca\r\nConnection: close\r\n\r\n"
 
     # try:
-    client = smart_web_client(h, port)
+    client = smart_web_client(host2, port)
     # except:
         # print "failed creating socket"
     
     # try:
-    client.connect()
+    # client.connect()
     # except:
         # print "failed connecting to {} on {}".format(host, port)
-    
+    print 'woop'
     # try:
-    response = client.send_request(request)
+    response = client.send_request(r2)
     # print response
     print 'test'
     # except Exception, e:
